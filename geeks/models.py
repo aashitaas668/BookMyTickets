8518552a246005d7491b7e87af29a4abdb173af6
aashitaas668 <aashitaas668@gmail.com>
2022-05-30 17:03:44 +0530

    Added my project

--- geeks/models.py
+++ geeks/models.py
@@ -1,28 +1,47 @@
 from django.db import models
 
 # Create your models here.
-class Flight(models.Model):
+class Flight_Details(models.Model):
     flight_number = models.CharField(max_length=10)
-    op_airline = models.CharField(max_length=10)
-    departure_city = models.CharField(max_length=10)
-    arrival_city = models.CharField(max_length=10)
+    flight_name = models.CharField(max_length=30)
+    arrival_time = models.TimeField()
+    departure_time = models.TimeField()
+    airline_name = models.CharField(max_length=10)
+    source = models.CharField(max_length=10)
+    destination = models.CharField(max_length=10)
+    source_airport = models.CharField(max_length=10)
+    destination_airport = models.CharField(max_length=10)
+    flight_duration = models.IntergerField()
+    stops = models.IntegerField()
+    remaining_seats = models.IntegerField()
+    total_seats = models.IntegerField()
+    price = models.Charfield(max_length=10)
     date_of_departure = models.DateField()
-    eta_departure = models.TimeField()
+   
 
     def __str__(self):
         return self.flight_number + " " + self.op_airline
 
-class Passenger(models.Model):
+class User(models.Model):
+    user_number = models.Charfield(max_length=10)
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
-    middlename = models.CharField(max_length=20)
     email = models.EmailField(max_length=20)
     phone = models.CharField(max_length=10)
+    password = models.CharField(max_length=20)
+    is_admin = models.BooleanField()
 
     def __str__(self):
         return self.firstname + " " + self.lastname
-
-
-class Reservation(models.Model):
-    flight = models.ForeignKey(Flight, on_delete=models.CASCADE)
+        
+    class Meta:
+      abstract = True
+class Booking(models.Model):
+    booking_id = models.CharField(max_length = 10)
+    booking_date = models.DateField()
+    trip_date = models.Date_Field()
+    flight_number = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    user_number = models.ForeignKey(Flight , on_delete=models.CASCADE)
+    number_of_passengers = models.IntergerField()
+    total_price = models.IntegerField()
     passenger = models.OneToOneField(Passenger, on_delete=models.CASCADE)
